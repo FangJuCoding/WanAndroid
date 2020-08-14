09760aa8b47b782b110566c541810f0a6c20a151
FangJu <fangjucoding@foxmail.com>
2020-08-14 12:24:36 +0800

    增加登陆检测拦截

--- library-base/src/main/java/com/funcoding/wanandroid/base/router/ARouterHelper.kt
+++ library-base/src/main/java/com/funcoding/wanandroid/base/router/ARouterHelper.kt
@@ -1,9 +1,15 @@
 package com.funcoding.wanandroid.base.router
 
 import com.alibaba.android.arouter.launcher.ARouter
-
+import com.funcoding.wanandroid.base.base.AppContext
+import com.funcoding.wanandroid.base.router.callback.LoginNavigationCallback
 
 object ARouterHelper {
+
+    fun navWithLoginInterceptorCallback(path: String) {
+        ARouter.getInstance().build(path).navigation(AppContext, LoginNavigationCallback())
+    }
+
     fun navigation(path: String) {
         ARouter.getInstance().build(path).navigation()
     }
---
+++ library-base/src/main/java/com/funcoding/wanandroid/base/router/interceptor/LoginInterceptor.kt
@@ -0,0 +1,37 @@
+package com.funcoding.wanandroid.base.router.interceptor
+
+import android.content.Context
+import com.alibaba.android.arouter.facade.Postcard
+import com.alibaba.android.arouter.facade.annotation.Interceptor
+import com.alibaba.android.arouter.facade.callback.InterceptorCallback
+import com.alibaba.android.arouter.facade.template.IInterceptor
+import com.funcoding.wanandroid.base.router.RouterPath
+import com.funcoding.wanandroid.base.router.service.account.AccountServiceImplGlobal
+import java.lang.RuntimeException
+
+/**
+ * 登陆检测拦截器
+ */
+@Interceptor(priority = 100, name = "登陆检测拦截器")
+class LoginInterceptor : IInterceptor {
+    override fun process(postcard: Postcard, callback: InterceptorCallback) {
+        val isLogin = AccountServiceImplGlobal.isLogin()
+        if (isLogin) {
+            callback.onContinue(postcard)
+        } else {
+            when (postcard.path) {
+                RouterPath.PAGER_ACTIVITY_ACCOUNT,
+                RouterPath.PAGER_ACTIVITY_MAIN
+                -> {
+                    callback.onContinue(postcard)
+                }
+                else -> {
+                    callback.onInterrupt(RuntimeException("需要登陆"))
+                }
+            }
+        }
+    }
+
+    override fun init(context: Context?) {
+    }
+}
